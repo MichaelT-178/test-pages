94e67f610bbe99979c1eed490af49e53e225faec
Michael Totaro <mstotaro@gmail.com>
2026-01-17 13:52:22 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <p class="test-header">TEST WEBSITE</p>
+  <router-view />
+</template>
+
+
+<script setup>
+
+</script>
+
+
+<style>
+*, *::before, *::after { 
+  box-sizing: border-box; 
+}
+
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+.test-header {
+  font-size: 25px;
+  padding-left: 20px;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <p class="title">HOME PAGE</p>
+  <LinkBtn
+    :path="'/second-page'"
+    :buttonText="'Go to 2nd page'">
+  </LinkBtn>
+</template>
+
+
+<script setup>
+import LinkBtn from '../components/RouterBtn.vue';
+</script>
+
+
+<style scoped>
+
+.title {
+  color: red;
+  font-family: Arial, sans-serif;
+  padding-left: 20px;
+}
+
+</style>
